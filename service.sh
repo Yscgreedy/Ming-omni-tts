80272f7ad66cbd18ead816ab42da9a3f29bdc00a
cnb.a0IYvXojAGA <sywog11JfXDln3owR5MLqG+cnb.a0IYvXojAGA@noreply.cnb.cool>
2026-03-30 11:34:06 +0800

    echo

--- service.sh
+++ service.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-NUM_WORKERS=8
+NUM_WORKERS=1
 BASE_PORT=8000
+echo $CNB_VSCODE_PROXY_URI
 for i in $(seq 1 $NUM_WORKERS); do
     PORT=$((BASE_PORT+i)) # 计算每个服务的端口号
     PORT=$PORT python ./service/app.py &
